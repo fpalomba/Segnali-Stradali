5b95bdfa00dc4fe8af6bd636b778869da2fa2d23
marcolongo00 <m.longo36@studenti.unisa.it>
2022-06-14 12:10:45 +0200

    Analisi e scrematura del dataset, formattazione dei dati con importazione delle librerie, estrazione delle feature, costruzione del dataframe e training e test set

--- implementazione/road_sign_classification.py
+++ implementazione/road_sign_classification.py
@@ -60,3 +60,56 @@ new_cols = ["f_name", "path_name", "width", "height", "depth", "class_name"]
 data = pd.DataFrame(data=content, columns=new_cols)
 data.class_name = data.class_name.map({'trafficlight': 1, 'speedlimit': 2, 'crosswalk': 3, 'stop': 4})
 print(data.shape)
+# data.head()
+
+print("Waiting. . .")
+data1 = []
+
+i = 0
+
+for a in data.path_name.values:
+    image = Image.open("..\\resources\\" + a).convert("RGB")
+
+    # Image resizing is needed to upgrade the resolution
+    image = image.resize((224, 224), Image.ANTIALIAS)
+    image = np.array(image.getdata()).reshape(224, 224, 3)
+    data1.append(image)
+
+print("---Done---")
+
+X = np.array(data1)
+
+y = np.array(data.iloc[:, -1], dtype=int)
+
+c = to_categorical(y, dtype=int)
+Y = c[:, 1:]
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=787)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# Layers definition
+model = Sequential()
+model.add(Conv2D(128, kernel_size=(3 ,3), activation='relu', input_shape=X_train.shape[1:]))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(rate=0.25))
+model.add(Dense(4, activation='softmax'))
+
+# Compilation of the model
+# categorical_crossentropy(multiclass classification problems)
+# Optimizer Adaptive Moment lr=0.001
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+history = model.fit(X_train, y_train, batch_size=64, epochs=50, validation_data=(X_test, y_test))
+
+# Evaluating
+results = model.evaluate(X_test, y_test, batch_size=64)
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
