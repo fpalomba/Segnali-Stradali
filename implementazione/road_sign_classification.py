236144de6fb4a31bc7ed01b007750277cab675ab
rikon311 <polidororiccardo84@gmail.com>
2022-10-14 17:45:05 +0200

    Commit del codice del modello

--- implementazione/road_sign_classification.py
+++ implementazione/road_sign_classification.py
@@ -1,115 +1,240 @@
-import bs4
-import os
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from PIL import Image, ImageTk
+import os
+import cv2
 import matplotlib.pyplot as plt
-
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
+import random
+from matplotlib.image import imread
+from matplotlib import style
+style.use('fivethirtyeight')
+np.random.seed(42)
 
 tf.random.set_seed(100)
 
-path = "..\\resources\\annotations"
-content = []
-speedcounter = 0
+data_dir = '../Segnali-Stradali/input'
+train_path = '../Segnali-Stradali/input/Train'
+test_path = '../Segnali-Stradali/input/'
 
-for filename in os.listdir(path):
+# Le immagini vengono ridimensionate con dimensioni 30x30x3
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3 
 
-    if not filename.endswith('.xml'): continue
-    finalpath = os.path.join(path, filename)
+NUM_CATEGORIES = len(os.listdir(train_path))
+print(NUM_CATEGORIES)
 
-    infile = open(finalpath, "r")
+# Label per le varie classi
+classes = {0: 'Speed limit (20km/h)',
+            1: 'Speed limit (30km/h)',
+            2: 'Speed limit (50km/h)',
+            3: 'Speed limit (60km/h)',
+            4: 'Speed limit (70km/h)',
+            5: 'Speed limit (80km/h)',
+            6: 'End of speed limit (80km/h)',
+            7: 'Speed limit (100km/h)',
+            8: 'Speed limit (120km/h)',
+            9: 'No passing',
+            10: 'No passing veh over 3.5 tons',
+            11: 'Right-of-way at intersection',
+            12: 'Priority road',
+            13: 'Yield',
+            14: 'Stop',
+            15: 'No vehicles',
+            16: 'Veh > 3.5 tons prohibited',
+            17: 'No entry',
+            18: 'General caution',
+            19: 'Dangerous curve left',
+            20: 'Dangerous curve right',
+            21: 'Double curve',
+            22: 'Bumpy road',
+            23: 'Slippery road',
+            24: 'Road narrows on the right',
+            25: 'Road work',
+            26: 'Traffic signals',
+            27: 'Pedestrians',
+            28: 'Children crossing',
+            29: 'Bicycles crossing',
+            30: 'Beware of ice/snow',
+            31: 'Wild animals crossing',
+            32: 'End speed + passing limits',
+            33: 'Turn right ahead',
+            34: 'Turn left ahead',
+            35: 'Ahead only',
+            36: 'Go straight or right',
+            37: 'Go straight or left',
+            38: 'Keep right',
+            39: 'Keep left',
+            40: 'Roundabout mandatory',
+            41: 'End of no passing',
+            42: 'End no passing veh > 3.5 tons'}
 
-    contents = infile.read()
-    # Usiamo la libreria BeautifulSoup per estrarre i dati dal file xml(altezza,larghezza e profondità dell'immagine)
-    soup = bs4.BeautifulSoup(contents, 'xml')
-    class_name = soup.find_all("name")
-    name = soup.find_all('filename')
-    width = soup.find_all("width")
-    height = soup.find_all("height")
-    depth = soup.find_all("depth")
 
-    ls = []
-    for x in range(0, len(name)):
-        for i in name:
-            name = name[x].get_text()
-            path_name = "images/" + name
 
-        class_name = class_name[x].get_text()
-        if class_name == 'speedlimit':
-            if speedcounter < 75:
-                # Counter per le immagini della classe 'limiti di velocità'
-                height = int(height[x].get_text())
-                depth = int(depth[x].get_text())
-                width = int(width[x].get_text())
-                f_name = filename
-                ls.extend([f_name, path_name, width, height, depth, class_name])
-                speedcounter = speedcounter + 1
-                content.append(ls)
-        else:
-            # Seleziona le altre classi di immagini(segnali di stop,attraversamenti pedonali,semafori)
-            height = int(height[x].get_text())
-            depth = int(depth[x].get_text())
-            width = int(width[x].get_text())
-            f_name = filename
-            ls.extend([f_name, path_name, width, height, depth, class_name])
-            content.append(ls)
+folders = os.listdir(train_path)
 
-new_cols = ["f_name", "path_name", "width", "height", "depth", "class_name"]
-data = pd.DataFrame(data=content, columns=new_cols)
-data.class_name = data.class_name.map({'trafficlight': 1, 'speedlimit': 2, 'crosswalk': 3, 'stop': 4})
-print(data.shape)
-# data.head()
+train_number = []
+class_num = []
 
-print("Waiting. . .")
-data1 = []
+for folder in folders:
+    train_files = os.listdir(train_path + '/' + folder)
+    train_number.append(len(train_files))
+    class_num.append(classes[int(folder)])
 
-i = 0
 
-for a in data.path_name.values:
-    image = Image.open("..\\resources\\" + a).convert("RGB")
+zipped_lists = zip(train_number, class_num)
+sorted_pairs = sorted(zipped_lists)
+tuples = zip(*sorted_pairs)
+train_number, class_num = [list(tuple) for tuple in tuples]
 
-    # Image resizing is needed to upgrade the resolution
-    image = image.resize((224, 224), Image.ANTIALIAS)
-    image = np.array(image.getdata()).reshape(224, 224, 3)
-    data1.append(image)
 
-print("---Done---")
-
-X = np.array(data1)
-
-y = np.array(data.iloc[:, -1], dtype=int)
-
-c = to_categorical(y, dtype=int)
-Y = c[:, 1:]
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=787)
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
-# Layers definition
-model = Sequential()
-model.add(Conv2D(128, kernel_size=(3 ,3), activation='relu', input_shape=X_train.shape[1:]))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(rate=0.25))
-model.add(Dense(4, activation='softmax'))
-
-# Compilation of the model
-# categorical_crossentropy(multiclass classification problems)
-# Optimizer Adaptive Moment lr=0.001
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-
-history = model.fit(X_train, y_train, batch_size=64, epochs=50, validation_data=(X_test, y_test))
-
-# Evaluating
-results = model.evaluate(X_test, y_test, batch_size=64)
-
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
+plt.figure(figsize=(21, 10))
+plt.bar(class_num, train_number)
+plt.xticks(class_num, rotation='vertical')
 plt.show()
+
+test = pd.read_csv(data_dir + '/Test.csv')
+# Leggiamo il path da dove prendere le immagini di test
+imgs = test["Path"].values
+
+plt.figure(figsize=(25, 25))
+
+# Visualizzazione di 25 immagini casuali prese dal test set
+for i in range(1, 26):
+    plt.subplot(5, 5, i)
+    random_img_path = data_dir + '/' + random.choice(imgs)
+    rand_img = imread(random_img_path)
+    plt.imshow(rand_img)
+    plt.grid(visible=None)
+    plt.xlabel(rand_img.shape[1], fontsize=20)   # larghezza dell'immagine
+    plt.ylabel(rand_img.shape[0], fontsize=20)   # altezza dell'immagine
+
+plt.show()
+
+
+# Collezioniamo i dati dal training set
+image_data = []
+image_labels = []
+
+for i in range(NUM_CATEGORIES):
+    path = data_dir + '/Train/' + str(i)
+    images = os.listdir(path)
+
+    for img in images:
+        try:
+            image = cv2.imread(path + '/' + img)
+            image_fromarray = Image.fromarray(image, 'RGB')
+            resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+            image_data.append(np.array(resize_image))
+            image_labels.append(i)
+        except:
+            print("Error in " + img)
+
+
+image_data = np.array(image_data)
+image_labels = np.array(image_labels)
+
+print(image_data.shape, image_labels.shape)
+
+# Mischiamo i dati di training
+shuffle_indexes = np.arange(image_data.shape[0])
+np.random.shuffle(shuffle_indexes)
+image_data = image_data[shuffle_indexes]
+image_labels = image_labels[shuffle_indexes]
+
+
+
+X_train, X_val, y_train, y_val = train_test_split(image_data, image_labels, test_size=0.3, random_state=42, shuffle=True)
+
+
+X_train = X_train/255
+X_val = X_val/255
+
+print("X_train.shape", X_train.shape)
+print("X_valid.shape", X_val.shape)
+print("y_train.shape", y_train.shape)
+print("y_valid.shape", y_val.shape)
+
+
+y_train = keras.utils.to_categorical(y_train, NUM_CATEGORIES)
+y_val = keras.utils.to_categorical(y_val, NUM_CATEGORIES)
+
+print(y_train.shape)
+print(y_val.shape)
+
+# Creazione modello
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu',
+                        input_shape=(IMG_HEIGHT, IMG_WIDTH, channels)),
+    keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+
+    keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(rate=0.5),
+
+    keras.layers.Dense(43, activation='softmax')
+])
+
+lr = 0.001
+epochs = 10
+
+opt = Adam(lr=lr, decay=lr / (epochs * 0.5))
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+aug = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.15,
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode="nearest")
+
+history = model.fit(aug.flow(X_train, y_train, batch_size=32), epochs=epochs, validation_data=(X_val, y_val))
+
+
+# Risultati accuratezza training
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
+
+test = pd.read_csv(data_dir + '/Test.csv')
+
+labels = test["ClassId"].values
+imgs = test["Path"].values
+
+data =[]
+
+for img in imgs:
+    try:
+        image = cv2.imread(data_dir + '/' +img)
+        image_fromarray = Image.fromarray(image, 'RGB')
+        resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+        data.append(np.array(resize_image))
+    except:
+        print("Error in " + img)
+X_test = np.array(data)
+
+pred = model.evaluate(X_test,labels)
+
+# Accuratezza modello
+print('Accuratezza: ',accuracy_score(labels, pred) * 100)
+
+
