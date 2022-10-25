24a66fcd1b9a97dae798192d5da29288c6e34bb5
rikon311 <polidororiccardo84@gmail.com>
2022-10-25 15:24:20 +0200

    Corretto codice per accuratezza test set

--- implementazione/road_sign_classification.py
+++ implementazione/road_sign_classification.py
@@ -215,26 +215,10 @@ plt.gca().set_ylim(0, 1)
 plt.show()
 
 
-test = pd.read_csv(data_dir + '/Test.csv')
 
-labels = test["ClassId"].values
-imgs = test["Path"].values
-
-data =[]
-
-for img in imgs:
-    try:
-        image = cv2.imread(data_dir + '/' +img)
-        image_fromarray = Image.fromarray(image, 'RGB')
-        resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
-        data.append(np.array(resize_image))
-    except:
-        print("Error in " + img)
-X_test = np.array(data)
-
-pred = model.evaluate(X_test,labels)
+pred = model.evaluate(X_val,y_val)
 
 # Accuratezza modello
-print('Accuratezza: ',accuracy_score(labels, pred) * 100)
+print(pred[1]);
 
 
